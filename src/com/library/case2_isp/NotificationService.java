6bc4fc0dc150a1dd1944082349e6b01149a0ed39
yasminakmila <165403248+yasminakmila@users.noreply.github.com>
2026-03-16 21:08:43 +0700

    Delete src/com/library/case2_isp/NotificationService.java

--- src/com/library/case2_isp/NotificationService.java
+++
@@ -1,7 +0,0 @@
-package com.library.case2_isp;
-
-public interface NotificationService {
-    void sendEmail(String message);
-    void sendSMS(String message);
-    void sendPushNotification(String message);
-}
