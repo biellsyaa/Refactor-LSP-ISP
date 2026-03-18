43d1ac11586e0b0637e1f43ee2b79fc0736597d8
nailazza <165487007+nailazza@users.noreply.github.com>
2026-03-18 22:28:49 +0700

    BasicPrinter.java

--- src/com/library/case3_isp/BasicPrinter.java
+++ src/com/library/case3_isp/BasicPrinter.java
@@ -1,19 +1,9 @@
 package com.library.case3_isp;
 
-public class BasicPrinter implements MultiFunctionMachine {
+public class BasicPrinter implements Printer {
 
     @Override
     public void print(String document) {
         System.out.println("Printing document: " + document);
-    }
-
-    @Override
-    public void scan(String document) {
-        throw new UnsupportedOperationException("BasicPrinter does not support scanning.");
-    }
-
-    @Override
-    public void fax(String document) {
-        throw new UnsupportedOperationException("BasicPrinter does not support faxing.");
     }
 }
