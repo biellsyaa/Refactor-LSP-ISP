451ad0897ffee29af7795ed91290d2d8324f0a32
nailazza <165487007+nailazza@users.noreply.github.com>
2026-03-18 22:30:42 +0700

    Printer.java

--- src/com/library/case3_isp/MultiFunctionMachine.java
+++
@@ -1,7 +0,0 @@
-package com.library.case3_isp;
-
-public interface MultiFunctionMachine {
-    void print(String document);
-    void scan(String document);
-    void fax(String document);
-}
---
+++ src/com/library/case3_isp/Printer.java
@@ -0,0 +1,5 @@
+package com.library.case3_isp;
+
+public interface Printer {
+    void print(String document);
+}
